10fd2025e5176979315becf7a6e866f4f658d636
KDora90 <kummer.dora90@gmail.com>
2023-08-18 12:04:12 +0200

    drawImage fuggveny

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,15 @@
-console.log("Hello, világ");
+const image = document.getElementById('hidden');
+const canvas = document.getElementById('myCanvas');
+const c = canvas.getContext('2d');
+
+const size = 50;
+let x = 100;
+let y = 50;
+
+drawImage(0, 0);
+drawImage(100,100);
+drawImage(123, 234);
+
+function drawImage(x, y){
+    c.drawImage(image, x, y,size, size);
+}
